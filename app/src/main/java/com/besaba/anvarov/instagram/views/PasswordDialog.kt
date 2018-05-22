d2067990383ad1b10fb097b80ab760fcae0a2a68
RatWar <aa960008>
2018-05-22 14:26:48 +0500

    15

---
+++ app/src/main/java/com/besaba/anvarov/instagram/views/PasswordDialog.kt
@@ -0,0 +1,36 @@
+package com.besaba.anvarov.instagram.views
+
+import android.app.Activity
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
+import com.besaba.anvarov.instagram.R
+import kotlinx.android.synthetic.main.dialog_password.view.*
+
+class PasswordDialog: DialogFragment() {
+    private lateinit var mListener: Listener
+
+    interface Listener {
+        fun onPasswordConfirm(password: String)
+    }
+
+    override fun onAttach(activity: Activity?) {
+        super.onAttach(activity)
+        mListener = context as Listener
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val view = activity!!.layoutInflater.inflate(R.layout.dialog_password, null)
+        return AlertDialog.Builder(context!!)
+                .setView(view)
+                .setPositiveButton(android.R.string.ok, {_, _ ->
+                    mListener.onPasswordConfirm(view.password_input.text.toString())
+                })
+                .setNegativeButton(android.R.string.cancel, {_, _ ->
+
+                })
+                .setTitle(R.string.please_enter_password)
+                .create()
+    }
+}
