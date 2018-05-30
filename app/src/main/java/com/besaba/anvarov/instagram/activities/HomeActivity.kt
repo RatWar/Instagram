c2c1550ad001a19c76d85537a6c14d2cf45cc428
RatWar <aa960008>
2018-05-30 15:56:47 +0500

    23

--- app/src/main/java/com/besaba/anvarov/instagram/activities/HomeActivity.kt
+++ app/src/main/java/com/besaba/anvarov/instagram/activities/HomeActivity.kt
@@ -2,34 +2,32 @@ package com.besaba.anvarov.instagram.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import com.besaba.anvarov.instagram.R
-import com.google.firebase.auth.FirebaseAuth
+import com.besaba.anvarov.instagram.utils.FirebaseHelper
+import com.besaba.anvarov.instagram.utils.GlideApp
+import com.besaba.anvarov.instagram.utils.ValueEventListenerAdapter
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.feed_item.view.*
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "HomeActivity"
-    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mFirebase: FirebaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        setupBottomNavigation()
         Log.d(TAG, "onCreate")
+        setupBottomNavigation()
 
-        mAuth = FirebaseAuth.getInstance()
-//        mAuth.signInWithEmailAndPassword("ra7bkru@gmail.com", "960008")
-//                .addOnCompleteListener {
-//                    if (it.isSuccessful) {
-//                        Log.d(TAG, "signIn: succes")
-//                    } else {
-//                        Log.e(TAG, "signIn: failure", it.exception)
-//                    }
-//                }
-        sign_out_text.setOnClickListener {
-            mAuth.signOut()
-        }
-        mAuth.addAuthStateListener {
+        mFirebase = FirebaseHelper(this)
+        mFirebase.auth.addAuthStateListener {
             if (it.currentUser == null) {
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
@@ -39,9 +37,40 @@ class HomeActivity : BaseActivity(0) {
 
     override fun onStart() {
         super.onStart()
-        if (mAuth.currentUser == null) {
+        val currentUser = mFirebase.auth.currentUser
+        if (currentUser == null) {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
+        } else {
+            mFirebase.database.child("feed-posts").child(currentUser.uid)
+                    .addValueEventListener(ValueEventListenerAdapter {
+                        val posts = it.children.map { it.getValue(FeedPost::class.java)!! }
+                        Log.d(TAG, "feedPosts: ${posts.first().timestampDate()}")
+                        feed_recycler.adapter = FeedAdapter(posts)
+                        feed_recycler.layoutManager = LinearLayoutManager(this)
+                    })
         }
+    }
+}
+
+class FeedAdapter(private val posts: List<FeedPost>)
+    : RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
+
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.feed_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.view.post_image.loadImage(posts[position].image)
+    }
+
+    override fun getItemCount() = posts.size
+
+    private fun ImageView.loadImage(image: String) {
+        GlideApp.with(this).load(image).centerCrop().into(this)
     }
 }
--- app/src/main/java/com/besaba/anvarov/instagram/activities/ShareActivity.kt
+++ app/src/main/java/com/besaba/anvarov/instagram/activities/ShareActivity.kt
@@ -1,16 +1,101 @@
 package com.besaba.anvarov.instagram.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.besaba.anvarov.instagram.R
+import com.besaba.anvarov.instagram.models.User
+import com.besaba.anvarov.instagram.utils.CameraHelper
+import com.besaba.anvarov.instagram.utils.FirebaseHelper
+import com.besaba.anvarov.instagram.utils.GlideApp
+import com.besaba.anvarov.instagram.utils.ValueEventListenerAdapter
+import com.google.firebase.database.ServerValue
+import kotlinx.android.synthetic.main.activity_share.*
+import java.util.*
 
 class ShareActivity : BaseActivity(2) {
     private val TAG = "ShareActivity"
+    private lateinit var mCamera: CameraHelper
+    private lateinit var mFirebase: FirebaseHelper
+    private lateinit var mUser: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        setupBottomNavigation()
+        setContentView(R.layout.activity_share)
         Log.d(TAG, "onCreate")
+
+        mFirebase = FirebaseHelper(this)
+
+        mCamera = CameraHelper(this)
+        mCamera.takeCameraPicture()
+
+        back_image.setOnClickListener { finish() }
+        share_text.setOnClickListener { share() }
+
+        mFirebase.currentUserReference().addValueEventListener(ValueEventListenerAdapter {
+            mUser = it.getValue(User::class.java)!!
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == mCamera.REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                GlideApp.with(this).load(mCamera.imageUri).centerCrop().into(post_image)
+            } else {
+                finish()
+            }
+        }
+    }
+
+    private fun share() {
+        val imageUri = mCamera.imageUri
+        if (imageUri != null) {
+            val uid = mFirebase.auth.currentUser!!.uid
+            mFirebase.storage.child("users").child(uid).child("images")
+                    .child(imageUri.lastPathSegment).putFile(imageUri).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val imageDownloadUrl = it.result.downloadUrl!!.toString()
+                            mFirebase.database.child("images").child(uid).push()
+                                    .setValue(imageDownloadUrl)
+                                    .addOnCompleteListener {
+                                        if (it.isSuccessful) {
+                                            mFirebase.database.child("feed-posts").child(uid)
+                                                    .push()
+                                                    .setValue(mkFeedPost(uid, imageDownloadUrl))
+                                                    .addOnCompleteListener {
+                                                        if (it.isSuccessful) {
+                                                            startActivity(Intent(this,
+                                                                    ProfileActivity::class.java))
+                                                            finish()
+                                                        }
+                                                    }
+                                        } else {
+                                            showToast(it.exception!!.message!!)
+                                        }
+                                    }
+                        } else {
+                            showToast(it.exception!!.message!!)
+                        }
+                    }
+        }
+    }
+
+    private fun mkFeedPost(uid: String, imageDownloadUrl: String): FeedPost {
+        return FeedPost(
+                uid = uid,
+                username = mUser.username,
+                image = imageDownloadUrl,
+                caption = caption_input.text.toString(),
+                photo = mUser.photo
+        )
     }
 }
+
+data class FeedPost(val uid: String = "", val username: String = "",
+                    val image: String = "", val likesCount: Int = 0, val commentsCount: Int = 0,
+                    val caption: String = "", val comments: List<Comment> = emptyList(),
+                    val timestamp: Any = ServerValue.TIMESTAMP, val photo: String? = null) {
+    fun timestampDate(): Date = Date(timestamp as Long)
+}
+
+data class Comment(val uid: String, val username: String, val text: String)
